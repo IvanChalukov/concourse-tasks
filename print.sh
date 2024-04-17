3eac163ef6d61242ee267c47548d15606bfd3ec1
Ivan Chalakov <ivan.chalakov@bundlearrows.com>
2024-04-17 22:59:13 +0300

    add push step

--- print.sh
+++ print.sh
@@ -27,7 +27,6 @@ for name in $names; do
     fi
 done
 
- # Commit and push changes to the repository
+# Commit and push changes to the repository
 git add "$greeted_file"
 git commit -m "Update greeted.txt"
-git push origin task-5
