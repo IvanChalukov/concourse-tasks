b212a736df2f2832f92c9f33ef68dc3629d7b396
Ivan Chalakov <ivan.chalakov@bundlearrows.com>
2024-04-17 22:36:28 +0300

    add script changes

--- print.sh
+++ print.sh
@@ -1,3 +1,33 @@
 #!/bin/sh
 
 echo "Hello Concourse"
+
+if [ ! "personas.json" ]; then
+    echo "Error: File 'personas.json' not found."
+    exit 1
+fi
+
+if [ ! "greeted.txt" ]; then
+    greeted_file="greeted.txt"
+    touch "$greeted_file"
+fi
+
+# Get all names
+names=$(jq -r '.[].name' personas.json)
+
+for name in $names; do
+    if ! grep -q "^$name$" "$greeted_file"; then
+        # Greet the person
+        echo "Hello to SAP, $name"
+        # Add the greeted person to the list
+        echo "$name" >> "$greeted_file"
+    else
+        # Output that the person is already welcomed
+        echo "Person $name is already welcomed"
+    fi
+done
+
+ # Commit and push changes to the repository
+git add "$greeted_file"
+git commit -m "Update greeted.txt"
+git push origin task-4
