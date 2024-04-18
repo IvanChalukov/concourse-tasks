ac95157d0bac1b6f7908b0a6e88abc4f16c0eec9
Ivan Chalakov <ivan.chalakov@bundlearrows.com>
2024-04-18 22:59:20 +0300

    add unittests

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
+import boto3
 import time
 import os.path
 import sys
 import logging
 from typing import List
 
-import boto3
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
@@ -24,8 +24,8 @@ def check_arguments():
 def initialize_state_file(directory: str) -> str:
     """
     Initialize state file in given directory.
-    :param directory: Directory path
-    :return: path to the state file
+    :param directory: Directory path where to store state.txt file
+    :return: Full path to the state file
     """
     state_filename = "state.txt"
     state_file_path = os.path.join(directory, state_filename)
@@ -36,7 +36,7 @@ def initialize_state_file(directory: str) -> str:
     return state_file_path
 
 
-def check_running_time(start_time: float):
+def check_elapsed_time(start_time: float):
     """
     Check if the script has been running for more than 15 minutes.
     :param start_time: The time when script is started.
@@ -47,8 +47,12 @@ def check_running_time(start_time: float):
         sys.exit(1)
 
 
-def get_uploaded_files(state_file_path: str):
-    """Read the state file and return a list of files already uploaded."""
+def get_uploaded_files(state_file_path: str) -> List:
+    """
+    Read the state file and return a list of files already uploaded.
+    :param state_file_path: The path to the state file storing information about previously uploaded files.
+    :return: A list of file names that have already been uploaded.
+    """
     try:
         with open(state_file_path) as f:
             files_uploaded = [line.strip() for line in f]
@@ -59,7 +63,7 @@ def get_uploaded_files(state_file_path: str):
         return []
 
 
-def upload_file(s3_client, file_path: str, bucket_name: str) -> bool:
+def upload_file(s3_client: boto3.client, file_path: str, bucket_name: str) -> bool:
     """
     Upload a file to the specified S3 bucket.
     :param s3_client: Boto Service client instance
@@ -77,7 +81,7 @@ def upload_file(s3_client, file_path: str, bucket_name: str) -> bool:
         return False
 
 
-def retrieve_bucket(s3_client, bucket_name: str) -> List:
+def retrieve_bucket(s3_client: boto3.client, bucket_name: str) -> List:
     """
     Retrieve the list of objects and their last modified dates in the specified S3 bucket.
     :param s3_client: Boto Service client instance
@@ -97,39 +101,54 @@ def retrieve_bucket(s3_client, bucket_name: str) -> List:
         return []
 
 
-def update_state_file(file_name, state_file):
-    """Add the uploaded file name to the state file."""
+def update_state_file(file_name: str, state_file: str):
+    """
+    Add the uploaded file name to the state file.
+    :param file_name: The name of the file to be added to the state file.
+    :param state_file: The path to the state file where the file name will be added.
+    """
     with open(state_file, 'a') as f:
         f.write(file_name + '\n')
-        logger.info("Updated state file with file: {}".format(file_name))
+        logger.info(f"Updated state file with file: {file_name}")
+
+
+def process_files(state_file_path: str, dir_name: str, bucket_name: str, s3_client: boto3.client) -> bool:
+    """
+    Process files in the specified directory for uploading to the S3 bucket.
+    :param state_file_path: The path to the state file storing information about previously uploaded files.
+    :param dir_name: The directory containing the files to be processed.
+    :param bucket_name: The name of the S3 bucket where files will be uploaded.
+    :param s3_client: The Boto3 S3 client instance.
+    :return: True if new files were uploaded during the process, False otherwise.
+    """
+    new_files_uploaded = False
+    uploaded_files = get_uploaded_files(state_file_path)
+
+    for file_name in os.listdir(dir_name):
+        if file_name in uploaded_files:
+            continue
+
+        file_path = os.path.join(dir_name, file_name)
+        if not os.path.isfile(file_path):
+            continue
+
+        if upload_file(s3_client, file_name, bucket_name):
+            update_state_file(file_name, state_file_path)
+            new_files_uploaded = True
+
+    return new_files_uploaded
 
 
 def main():
     check_arguments()
     state_file_path = initialize_state_file('./')
     dir_name = sys.argv[1]
-    # Consider reading of aws_access_key_id, aws_secret_access_key, aws_region from env vars
-    # instead of .aws/credentials file
     s3 = boto3.client('s3')
     bucket_name = "candidatetask"
 
     start_time = time.time()
     while True:
-        check_running_time(start_time)
-        new_files_uploaded = False
-        uploaded_files = get_uploaded_files(state_file_path)
-
-        for file_name in os.listdir(dir_name):
-            if file_name in uploaded_files:
-                continue
-
-            file_path = os.path.join(dir_name, file_name)
-            if not os.path.isfile(file_path):
-                continue
-
-            if upload_file(s3, file_name, bucket_name):
-                update_state_file(file_name, state_file_path)
-                new_files_uploaded = True
+        new_files_uploaded = process_files(state_file_path, dir_name, bucket_name, s3)
 
         if not new_files_uploaded:
             logger.info("No new files to upload.")
@@ -137,6 +156,7 @@ def main():
             logger.info(
                 f"New files uploaded to {bucket_name}. Current bucket contents: {retrieve_bucket(s3, bucket_name)}")
 
+        check_elapsed_time(start_time)
         time.sleep(10)
 
 
--- tests_script.py
+++ tests_script.py
@@ -1,10 +1,14 @@
+import datetime
 import logging
 import os
 import sys
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
-from script import check_arguments, initialize_state_file, check_running_time
+from dateutil.tz import tzutc
+
+from script import check_arguments, initialize_state_file, check_elapsed_time, get_uploaded_files, upload_file, \
+    retrieve_bucket, update_state_file, main, process_files
 
 
 class TestScript(unittest.TestCase):
@@ -55,10 +59,9 @@ class TestScript(unittest.TestCase):
     @patch('script.open', create=True)
     @patch('script.logger')
     def test_initialize_state_file_exist(self, mock_logger, mock_open, mock_path):
-        directory = "/tests_dir"
         mock_path.isfile.return_value = True
 
-        initialize_state_file(directory)
+        initialize_state_file(self.tests_dir)
 
         mock_open.assert_not_called()
         mock_logger.info.assert_not_called()
@@ -66,10 +69,10 @@ class TestScript(unittest.TestCase):
     @patch('script.sys.exit')
     @patch('script.logger')
     @patch('script.time.time')
-    def test_check_running_time_less_than_15_minutes(self, mock_current_time, mock_logger, mock_exit):
+    def test_check_elapsed_time_less_than_15_minutes(self, mock_current_time, mock_logger, mock_exit):
         start_time = 1000
         mock_current_time.return_value = start_time + 600
-        check_running_time(start_time)
+        check_elapsed_time(start_time)
 
         mock_exit.assert_not_called()
         mock_logger.assert_not_called()
@@ -77,14 +80,196 @@ class TestScript(unittest.TestCase):
     @patch('script.sys.exit')
     @patch('script.logger')
     @patch('script.time.time')
-    def test_check_running_time_less_than_15_minutes(self, mock_current_time, mock_logger, mock_exit):
+    def test_check_elapsed_time_more_than_15_minutes(self, mock_current_time, mock_logger, mock_exit):
         start_time = 1000
         mock_current_time.return_value = start_time + 1200
 
-        check_running_time(start_time)
+        check_elapsed_time(start_time)
 
         mock_logger.error.assert_called_once_with("Script has been running for more than 15 minutes. Exiting...")
         mock_exit.assert_called_once_with(1)
+
+    @patch('script.open', create=True)
+    @patch('script.logger')
+    @patch('script.os.path.isfile', return_value=True)
+    def test_get_uploaded_files_success(self, mock_isfile, mock_logger, mock_open):
+        state_file_path = 'state.txt'
+        get_uploaded_files(state_file_path)
+        mock_open.assert_called_once_with(state_file_path)
+        mock_logger.info.assert_called_once_with(
+            "Successfully retrieved the list of files that have already been uploaded.")
+
+    @patch('script.open', side_effect=FileNotFoundError)
+    @patch('script.logger')
+    @patch('script.os.path.isfile', return_value=False)
+    def test_get_uploaded_files_file_not_found(self, mock_isfile, mock_logger, mock_open):
+        state_file_path = 'state.txt'
+        uploaded_files = get_uploaded_files(state_file_path)
+        self.assertEqual(uploaded_files, [])
+        mock_logger.error.assert_called_once_with(f"State file not found: {state_file_path}")
+
+    @patch('script.logger')
+    @patch('script.os.path.basename', return_value='file.txt')
+    def test_upload_file_success(self, mock_basename, mock_logger):
+        s3_client = MagicMock()
+        s3_client.upload_file.return_value = None
+
+        mocked_file_path = '/path/to/file.txt'
+        mocked_bucket_name = 'test_bucket'
+
+        result = upload_file(s3_client, mocked_file_path, mocked_bucket_name)
+
+        self.assertTrue(result)
+        s3_client.upload_file.assert_called_once_with(mocked_file_path, mocked_bucket_name, 'file.txt')
+        mock_logger.info.assert_called_once_with(f"Uploaded file: {mocked_file_path} to bucket: {mocked_bucket_name}")
+
+    @patch('script.logger')
+    @patch('script.os.path.basename', return_value='file.txt')
+    def test_upload_file_failure(self, mock_basename, mock_logger):
+        s3_client = MagicMock()
+        s3_client.upload_file.side_effect = Exception('Upload failed')
+
+        mocked_file_path = '/path/to/file.txt'
+        mocked_bucket_name = 'test_bucket'
+
+        result = upload_file(s3_client, mocked_file_path, mocked_bucket_name)
+
+        self.assertFalse(result)
+        s3_client.upload_file.assert_called_once_with(mocked_file_path, mocked_bucket_name, 'file.txt')
+        mock_logger.error.assert_called_once_with(f"Failed to upload {mocked_file_path}: Upload failed")
+
+    @patch('script.logger')
+    def test_retrieve_bucket_success_empty_content(self, mock_logger):
+        # Mocking boto3 client
+        s3_client = MagicMock()
+        s3_client.list_objects_v2.return_value = {}
+
+        bucket_name = 'test_bucket'
+
+        result = retrieve_bucket(s3_client, bucket_name)
+
+        self.assertEqual(result, [])
+        s3_client.list_objects_v2.assert_called_once_with(Bucket=bucket_name)
+        mock_logger.info.assert_called_once_with(f"The bucket {bucket_name} is empty.")
+
+    @patch('script.logger')
+    def test_retrieve_bucket_success_content(self, mock_logger):
+        s3_client = MagicMock()
+        s3_client.list_objects_v2.return_value = {
+            'ResponseMetadata': {'RequestId': 'testRequestId', 'RetryAttempts': 1}, 'Contents': [
+                {'Key': 'Concourse_ID_1011.md',
+                 'LastModified': datetime.datetime(2024, 4, 18, 14, 26, 46, tzinfo=tzutc()),
+                 'ETag': '"eefd009945205ed786a9e61274a316e8"', 'Size': 8407, 'StorageClass': 'STANDARD'}],
+            'Name': 'candidatetask', 'Prefix': '', 'MaxKeys': 1000, 'EncodingType': 'url', 'KeyCount': 1}
+
+        bucket_name = 'test_bucket'
+
+        result = retrieve_bucket(s3_client, bucket_name)
+
+        self.assertEqual(result, [('Concourse_ID_1011.md', 'LastModified: 2024-04-18 14:26:46')])
+        s3_client.list_objects_v2.assert_called_once_with(Bucket=bucket_name)
+
+    @patch('script.logger')
+    def test_retrieve_bucket_failure(self, mock_logger):
+        s3_client = MagicMock()
+        s3_client.list_objects_v2.side_effect = Exception('List files failed')
+
+        bucket_name = 'test_bucket'
+
+        result = retrieve_bucket(s3_client, bucket_name)
+
+        self.assertEqual(result, [])
+        s3_client.list_objects_v2.assert_called_once_with(Bucket=bucket_name)
+        mock_logger.error.assert_called_once_with(f"Failed to retrieve bucket {bucket_name}: List files failed")
+
+    @patch('script.open', create=True)
+    @patch('script.logger')
+    def test_update_state_file(self, mock_logger, mock_open):
+        mocked_file_name = "test_file.md"
+        mocked_state_file = 'state.txt'
+
+        update_state_file(mocked_file_name, mocked_state_file)
+        mock_open.assert_called_once_with(mocked_state_file, 'a')
+        mock_logger.info.assert_called_once_with(f"Updated state file with file: {mocked_file_name}")
+
+    @patch('script.get_uploaded_files', return_value=['file1.txt'])
+    @patch('os.listdir', return_value=['file1.txt', 'file2.txt', 'file3.txt'])
+    @patch('script.os.path.isfile', return_value=True)
+    @patch('script.upload_file', return_value=True)
+    @patch('script.update_state_file')
+    def test_process_files_with_new_files_uploaded(self, mock_update_state_file, mock_upload_file, mock_isfile,
+                                                   mock_listdir, mock_get_uploaded_files):
+        state_file_path = '/path/to/state.txt'
+        dir_name = '/path/to/directory'
+        bucket_name = 'my_bucket'
+        s3_client = MagicMock()
+
+        result = process_files(state_file_path, dir_name, bucket_name, s3_client)
+
+        self.assertTrue(result)
+        mock_get_uploaded_files.assert_called_once_with(state_file_path)
+        mock_listdir.assert_called_once_with(dir_name)
+        mock_isfile.assert_called()
+        mock_upload_file.assert_any_call(s3_client, 'file2.txt', bucket_name)
+        mock_upload_file.assert_any_call(s3_client, 'file3.txt', bucket_name)
+        mock_update_state_file.assert_any_call('file2.txt', state_file_path)
+        mock_update_state_file.assert_any_call('file3.txt', state_file_path)
+
+    @patch('script.get_uploaded_files', return_value=['file1.txt', 'file2.txt', 'file3.txt'])
+    @patch('os.listdir', return_value=['file1.txt', 'file2.txt', 'file3.txt'])
+    @patch('script.os.path.isfile', return_value=True)
+    @patch('script.upload_file', return_value=False)
+    @patch('script.update_state_file')
+    def test_process_files_with_no_new_files_uploaded(self, mock_update_state_file, mock_upload_file, mock_isfile,
+                                                      mock_listdir, mock_get_uploaded_files):
+        state_file_path = '/path/to/state.txt'
+        dir_name = '/path/to/directory'
+        bucket_name = 'my_bucket'
+        s3_client = MagicMock()
+
+        result = process_files(state_file_path, dir_name, bucket_name, s3_client)
+
+        self.assertFalse(result)
+        mock_get_uploaded_files.assert_called_once_with(state_file_path)
+        mock_listdir.assert_called_once_with(dir_name)
+        # mock_isfile.assert_called()
+        mock_upload_file.assert_not_called()
+        mock_update_state_file.assert_not_called()
+
+    @patch('script.sys.argv', ['script.py', 'test_directory'])
+    @patch('script.check_elapsed_time', side_effect=SystemExit)
+    @patch('script.boto3.client')
+    @patch('script.time.time')
+    @patch('script.check_arguments')
+    @patch('script.initialize_state_file')
+    @patch('script.process_files', return_value=False)
+    @patch('script.retrieve_bucket')
+    @patch('script.logger')
+    def test_main_no_new_files(self, mock_logger, mock_retrieve_bucket, mock_process_files, mock_initialize_state_file,
+                               mock_check_arguments, mock_time, mock_boto3, mock_check_elapsed_time):
+        with self.assertRaises(SystemExit) as cm:
+            main()
+
+        mock_logger.info.assert_called_once_with("No new files to upload.")
+
+    @patch('script.sys.argv', ['script.py', 'test_directory'])
+    @patch('script.check_elapsed_time', side_effect=SystemExit)
+    @patch('script.boto3.client')
+    @patch('script.time.time')
+    @patch('script.check_arguments')
+    @patch('script.initialize_state_file')
+    @patch('script.process_files', return_value=True)
+    @patch('script.retrieve_bucket', return_value='mocked_bucket_contents')
+    @patch('script.logger')
+    def test_main_with_new_files(self, mock_logger, mock_retrieve_bucket, mock_process_files,
+                                 mock_initialize_state_file, mock_check_arguments, mock_time, mock_boto3,
+                                 mock_check_elapsed_time):
+        with self.assertRaises(SystemExit) as cm:
+            main()
+
+        mock_retrieve_bucket.assert_called_once()
+        mock_logger.info.assert_called_once_with(
+            "New files uploaded to candidatetask. Current bucket contents: mocked_bucket_contents")
 
 
 if __name__ == '__main__':
